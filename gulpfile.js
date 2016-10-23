3aef8b12e141e666187492286d9cce227fe38a62
masaki-kamachi <masaki.kamachi@bizreach.co.jp>
2016-10-23 16:34:03 +0900

    sassビルド時にgulpが落ちないように修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber');
 
-gulp.task('sass', function(){
-  gulp.src('./css/*.scss')
+gulp.task('sass', () => {
+  return gulp.src('./css/*.scss')
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err.messageFormatted);
+        this.emit('end');
+      }
+    }))
     .pipe(sass())
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest('dist'))
+  ;
 });
 
-gulp.task('watch', function(){
-  gulp.watch('./css/*.scss', ['sass']);
-});
+gulp.task('watch', () => gulp.watch('./css/*.scss', ['sass']));
 
 gulp.task('default', ['sass']);
